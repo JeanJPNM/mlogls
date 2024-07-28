49e2d909be5052965e3aa0687c8b28a61f9c09ae
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2024-07-28 17:05:13 -0300

    fix: formatting breaks when indenting with tabs
    This happened because the parser entered statement mode whenever it found a tab, instead of ignoring it. This caused the start character of every syntax node to be
    zero whenever tabs were used for indentation

--- packages/mlogls/src/parser/tokenize.ts
+++ packages/mlogls/src/parser/tokenize.ts
@@ -56,6 +56,7 @@ export function tokenize(chars: string) {
         break;
       case ";":
       case " ":
+      case "\t":
       case "\r":
         pos++; //skip newlines and spaces
         break;
