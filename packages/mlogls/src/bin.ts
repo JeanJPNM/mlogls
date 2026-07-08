23229dd8fea0498caebb5d478f2d820bdd8a714a
JeanJPNM <61994401+JeanJPNM@users.noreply.github.com>
2026-07-08 11:31:44 -0300

    feat(cli): check command
    Closes #4.

--- packages/mlogls/src/bin.ts
+++ packages/mlogls/src/bin.ts
@@ -1,7 +1,17 @@
-import { createConnection, ProposedFeatures } from "vscode-languageserver/node";
+import {
+  createConnection,
+  Diagnostic,
+  DiagnosticSeverity,
+  ProposedFeatures,
+} from "vscode-languageserver/node";
 import { startServer } from "./server";
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
+import * as fs from "fs";
+import { MlogDocument } from "./document";
+import { pathToFileURL } from "url";
+import { getDocumentDiagnostics } from "./lsp/diagnostics";
+import pc from "picocolors";
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 yargs(hideBin(process.argv))
@@ -56,6 +66,126 @@ yargs(hideBin(process.argv))
       startServer({ connection });
     }
   )
+  .command(
+    "check <file>",
+    "Check a file for errors",
+    (yargs) => {
+      return yargs
+        .positional("file", {
+          type: "string",
+          desc: "The file to check",
+          demandOption: true,
+        })
+        .option("level", {
+          type: "string",
+          desc: "The level of diagnostics to report",
+          default: "info",
+          choices: ["hint", "info", "warning", "error"],
+        });
+    },
+    (args) => {
+      const { file } = args;
+
+      if (!fs.existsSync(file) || !fs.statSync(file).isFile()) {
+        console.error(pc.red(`Error: File does not exist at path: ${file}`));
+        process.exit(1);
+      }
+
+      const uri = pathToFileURL(file).toString();
+      const content = fs.readFileSync(file, "utf8");
+      const doc = new MlogDocument(uri, "mlog", 0, content);
+
+      const diagnostics = getDocumentDiagnostics(doc);
+
+      // diagnostics are not perfectly sorted because some are only
+      // added on later phases, such as diagnostics about
+      // diagnostic suppression
+      diagnostics.sort((a, b) => {
+        const aStart = a.range.start;
+        const bStart = b.range.start;
+        if (aStart.line === bStart.line) {
+          return aStart.character - bStart.character;
+        }
+        return aStart.line - bStart.line;
+      });
+
+      const filterLevel = stringToDiagnosticSeverity(args.level);
+      let hasErrors = false;
+
+      for (const diagnostic of diagnostics) {
+        const severity = diagnostic.severity ?? DiagnosticSeverity.Error;
+
+        if (severity > filterLevel) continue;
+        if (severity === DiagnosticSeverity.Error) {
+          hasErrors = true;
+        }
+
+        formatDiagnostic(doc, file, diagnostic);
+      }
+
+      process.exit(hasErrors ? 1 : 0);
+    }
+  )
+  .version()
   .help()
   .scriptName("mlogls")
   .parse();
+
+function stringToDiagnosticSeverity(level: string): DiagnosticSeverity {
+  switch (level) {
+    case "hint":
+      return DiagnosticSeverity.Hint;
+    case "info":
+      return DiagnosticSeverity.Information;
+    case "warning":
+      return DiagnosticSeverity.Warning;
+    case "error":
+      return DiagnosticSeverity.Error;
+    default:
+      return DiagnosticSeverity.Information;
+  }
+}
+
+function formatDiagnostic(
+  doc: MlogDocument,
+  filepath: string,
+  diagnostic: Diagnostic
+): void {
+  const location =
+    pc.blue(filepath) +
+    ":" +
+    pc.yellow(diagnostic.range.start.line + 1) +
+    ":" +
+    pc.yellow(diagnostic.range.start.character + 1);
+
+  let severity = pc.red("error");
+  if (diagnostic.severity === DiagnosticSeverity.Warning) {
+    severity = pc.yellow("warning");
+  } else if (diagnostic.severity === DiagnosticSeverity.Information) {
+    severity = pc.blue("info");
+  } else if (diagnostic.severity === DiagnosticSeverity.Hint) {
+    severity = pc.cyan("hint");
+  }
+
+  const code = pc.gray(diagnostic.code ? ` [${diagnostic.code}]` : "");
+  const line = doc
+    .getText({
+      start: { line: diagnostic.range.start.line, character: 0 },
+      end: { line: diagnostic.range.start.line + 1, character: 0 },
+    })
+    .trimEnd();
+  const markerLength =
+    diagnostic.range.start.line === diagnostic.range.end.line
+      ? diagnostic.range.end.character - diagnostic.range.start.character
+      : line.length - diagnostic.range.start.character;
+  const marker = "^".repeat(Math.max(1, markerLength));
+  // preserve tabs and replace everything else with spaces
+  // to keep the marker aligned with the code
+  const padding = line
+    .substring(0, diagnostic.range.start.character)
+    .replace(/[^\t]/g, " ");
+
+  console.log(`${location} - ${severity}${code}: ${diagnostic.message}`);
+  console.log(pc.gray(line));
+  console.log(pc.gray(padding + marker + "\n"));
+}
